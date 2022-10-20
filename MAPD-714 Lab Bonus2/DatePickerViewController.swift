dc4fb01ac625263f2205a750c162bc8c220a539d
patzkatigbak <patzkatigbak@gmail.com>
2022-10-19 21:50:53 -0400

    DatePicker View Controller added

--- MAPD-714 Lab Bonus2/DatePickerViewController.swift
+++ MAPD-714 Lab Bonus2/DatePickerViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class DatePickerViewController: UIViewController {
 
+    @IBOutlet var DatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let date = NSDate()
+                DatePicker.setDate(date as Date, animated: false)
     }
 
 
+    @IBAction func SelectButon_Pressed(_ sender: UIButton) {
+        let date = DatePicker.date
+                let message = "The date and time you selected is \(date)"
+                let alert = UIAlertController(
+                    title: "Date and Time Selected",
+                    message: message,
+                    preferredStyle: .alert)
+                let action = UIAlertAction(
+                    title: "That's so true!",
+                    style: .default,
+                    handler: nil)
+                alert.addAction(action)
+                present(alert, animated: true, completion: nil)
+    }
 }
 
