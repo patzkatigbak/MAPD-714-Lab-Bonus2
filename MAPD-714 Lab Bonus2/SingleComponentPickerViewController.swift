7fe30df808088b0309e8c8115447e286575bd4ce
patzkatigbak <patzkatigbak@gmail.com>
2022-10-19 21:58:43 -0400

    Added Single Component Screen

--- MAPD-714 Lab Bonus2/SingleComponentPickerViewController.swift
+++ MAPD-714 Lab Bonus2/SingleComponentPickerViewController.swift
@@ -7,8 +7,15 @@
 
 import UIKit
 
-class SingleComponentPickerViewController: UIViewController {
+class SingleComponentPickerViewController: UIViewController,
+                                           UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var singlePicker: UIPickerView!
+    
+    private let characterNames = [
+            "Luke", "Leia", "Han", "Chewbacca", "Artoo",
+            "Threepio", "Lando"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +23,36 @@ class SingleComponentPickerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func submitButton_Pressed(_ sender: UIButton) {
+        
+        let row = singlePicker.selectedRow(inComponent: 0)
+        let selected = characterNames[row]
+        let title = "You selected \(selected)!"
+        
+        let alert = UIAlertController(
+            title: title,
+            message: "Thank you for choosing",
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "You're welcome",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
-    */
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView,
+                        numberOfRowsInComponent component: Int) -> Int {
+            return characterNames.count
+        }
+        
+    
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return characterNames[row]
+        }
+    
 }
